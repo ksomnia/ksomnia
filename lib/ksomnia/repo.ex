78fb87b4438159e1e2fc3c74d41a2be6b10607f4
Evgenii Makrasov <emakrashov@gmail.com>
2022-06-10 09:36:18 +0300

    Use DATABASE_URL for the database connection

--- lib/ksomnia/repo.ex
+++ lib/ksomnia/repo.ex
@@ -2,4 +2,18 @@ defmodule Ksomnia.Repo do
   use Ecto.Repo,
     otp_app: :ksomnia,
     adapter: Ecto.Adapters.Postgres
+
+  require Logger
+
+  def init(_type, config) do
+    database_url = System.get_env("DATABASE_URL")
+
+    if database_url do
+      Logger.debug "Using $DATABASE_URL to configure the database connection"
+      {:ok, Keyword.put(config, :url, database_url)}
+    else
+      Logger.warn "$DATABASE_URL is not set"
+      {:ok, config}
+    end
+  end
 end
