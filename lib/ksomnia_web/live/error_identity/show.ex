0fb2a2584a7f315b931af7645827588a7b93357f
Evgenii Makrasov <emakrashov@gmail.com>
2022-09-18 07:25:14 +0300

    WIP

--- lib/ksomnia_web/live/error_identity/show.ex
+++ lib/ksomnia_web/live/error_identity/show.ex
@@ -61,7 +61,7 @@ defmodule KsomniaWeb.ErrorIdentityLive.Show do
     {:noreply, socket}
   end
 
-  def code_snippet(mappings, current_line, mapped_stacktrace) do
+  def code_snippet(mappings, current_line, error_identity_message) do
     max_line_num_len =
       Enum.map(mappings, & &1["line"])
       |> case do
@@ -81,35 +81,33 @@ defmodule KsomniaWeb.ErrorIdentityLive.Show do
             class: "text-slate-400"
           )
 
-        left =
-          content_tag(:span, [line_num, left_half],
-            class: "whitespace-pre-wrap border-r-2 border-red-300 py-1"
-          )
+        left = content_tag(:span, [line_num, left_half], class: "border-r-2 border-red-300 py-1")
 
         right_half = String.slice(formatted_line, line["column"], String.length(formatted_line))
 
         source = content_tag(:span, " #{Path.basename(line["source"])}", class: "text-slate-400")
 
-        right = content_tag(:span, [right_half, source], class: "whitespace-pre-wrap py-1")
+        right = content_tag(:span, [right_half, source], class: "py-1")
 
         content_tag(:div, [left, right],
           class:
-            "block group w-full hover:bg-indigo-100 cursor-pointer overflow-x-auto" <>
+            "group w-full hover:bg-indigo-100 cursor-pointer whitespace-nowrap " <>
               if(current_line == i, do: " bg-indigo-100", else: ""),
           "phx-click": "set_line_context",
           "phx-value-line": i
         )
       end
 
-    message = content_tag(:div, mapped_stacktrace, class: "block group w-full text-xs mb-1")
+    message = content_tag(:div, error_identity_message, class: "block group w-full text-xs mb-1 ")
 
     code = content_tag(:code, [message | lines])
-    content_tag(:pre, code, class: "mt-5 code-snippet")
+    content_tag(:pre, code, class: "code-snippet px-2")
   end
 
-  def render_line_source_context(mappings, current_line, source) do
+  def render_line_source_context(mappings, current_line, source, error_identity_message) do
     current_line_map = Enum.at(mappings, current_line)
     source_file_name = current_line_map["source"]
+    dbg(current_line_map)
 
     max_line_num_len =
       Enum.map(source, fn {_, i} -> i end)
@@ -128,35 +126,55 @@ defmodule KsomniaWeb.ErrorIdentityLive.Show do
           content_tag(
             :span,
             String.pad_leading("#{i + 1} ", max_line_num_len, " "),
-            class: "text-slate-400"
-          )
-
-        code_line =
-          content_tag(:span, if(line == "", do: " ", else: line),
-            class: "whitespace-pre-wrap py-1"
+            class: "text-slate-400 select-none"
           )
 
         if i + 1 == current_line_map["line"] do
-          content_tag(:div, [line_num, code_line],
-            class: "block group w-full hover:bg-indigo-100 cursor-pointer bg-indigo-100"
+          code_line_left_half = String.slice(line, 0, current_line_map["column"])
+
+          code_line_right_half =
+            String.slice(line, current_line_map["column"], String.length(line))
+
+          code_line_left_tag =
+            content_tag(:span, [line_num, code_line_left_half],
+              class: "border-r-2 border-orange-400 py-1"
+            )
+
+          code_line_right_tag = content_tag(:span, [code_line_right_half], class: "py-1")
+
+          hint =
+            content_tag(:div, error_identity_message,
+              class:
+                "absolute -top-2 left-7 text-xs bg-orange-200 text-orange-900 opacity-70 px-1 rounded-xs select-none invisible group-hover:visible "
+            )
+
+          content_tag(:div, [code_line_left_tag, code_line_right_tag, hint],
+            class: "group w-full bg-orange-100 whitespace-pre-wrap pl-2 relative "
           )
         else
-          content_tag(:div, [line_num, code_line],
-            class: "block group w-full hover:bg-indigo-100 cursor-pointer"
-          )
+          code_line =
+            content_tag(
+              :span,
+              if(line == "", do: " ", else: line),
+              class: "whitespace-pre-wrap py-1"
+            )
+
+          content_tag(:div, [line_num, code_line], class: "group w-full pl-2")
         end
       end
 
     message =
-      content_tag(:div, source_file_name, class: "block group w-full text-xs mb-1 text-slate-400")
+      content_tag(:div, source_file_name,
+        class: "block group w-full text-xs mb-1 text-slate-400 whitespace-nowrap pl-2 "
+      )
 
     code = content_tag(:code, [message | lines])
-    content_tag(:pre, code, class: "mt-5 code-snippet")
+    content_tag(:pre, code, class: "code-snippet")
   end
 
   def bare_code_snippet(content) do
     code = content_tag(:code, content)
-    content_tag(:pre, code, class: "mt-5 code-snippet")
+    content_tag(:pre, code, class: "code-snippet px-2")
   end
 
   defp page_title(:show), do: "Show App"
