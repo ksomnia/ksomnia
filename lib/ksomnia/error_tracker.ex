e44d899afe007e6eab398d6cb7e0b7a62eafcd06
Evgenii Makrashov <emakrashov@gmail.com>
2023-02-03 18:21:02 +0400

    Index errors in Meilisearch when tracking

--- lib/ksomnia/error_tracker.ex
+++ lib/ksomnia/error_tracker.ex
@@ -5,11 +5,13 @@ defmodule Ksomnia.ErrorTracker do
   alias Ksomnia.Repo
 
   def track(params) do
-    with {:app, %App{} = app} <- {:app, Repo.get_by(App, token: params["token"])} |> IO.inspect(),
+    with {:app, %App{} = app} <- {:app, Repo.get_by(App, token: params["token"])},
          {_, {:ok, error_identity}} <-
-           {:error_identity, ErrorIdentity.create(app, params)} |> IO.inspect(),
+           {:error_identity, ErrorIdentity.create(app, params)},
          {:tracked, _} <-
-           {:tracked, ErrorRecord.track(app, error_identity, params)} |> IO.inspect() do
+           {:tracked, ErrorRecord.track(app, error_identity, params)} do
+
+      Ksomnia.Meilisearch.create(error_identity, app.team_id)
       :ok
     end
   end
